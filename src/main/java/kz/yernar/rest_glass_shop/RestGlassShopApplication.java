9326b6fa663e42269721e68be2e97cda29399e3f
Yernar Yerzhan <77793457+Ernar2002@users.noreply.github.com>
2022-06-10 03:04:38 +0600

    Initial spring project

---
+++ src/main/java/kz/yernar/rest_glass_shop/RestGlassShopApplication.java
@@ -0,0 +1,13 @@
+package kz.yernar.rest_glass_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestGlassShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestGlassShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/yernar/rest_glass_shop/RestGlassShopApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.yernar.rest_glass_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestGlassShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
